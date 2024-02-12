3a5a316a381938e7f64ab495822fac2f15779d10
Artem Martynov <amerta90@mail.ru>
2024-02-12 10:52:18 +0300

    init commit

---
+++ FNSI/src/main/java/com/project/fnsi/FnsiApplication.java
@@ -0,0 +1,13 @@
+package com.project.fnsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnsiApplication.class, args);
+    }
+
+}
---
+++ FNSI/src/test/java/com/project/fnsi/FnsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fnsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

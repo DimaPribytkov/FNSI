86268291380359bd9659606f8afdf2b466129b70
Artem Martynov <amerta90@mail.ru>
2024-02-29 13:12:26 +0300

    fix sql request

--- FNSI/src/main/java/com/project/fnsi/dao/MappingRepository.java
+++ FNSI/src/main/java/com/project/fnsi/dao/MappingRepository.java
@@ -9,11 +9,13 @@ import java.util.Optional;
 
 public interface MappingRepository extends JpaRepository<Mapping, Long> {
 
-    @Query(value = "SELECT m FROM Mapping m WHERE " +
-            "CASE " +
-            "    WHEN (SELECT COUNT(*) FROM Mapping WHERE system = :system AND version = :version) > 0 " +
-            "    THEN (m.system = :system AND m.version = :version) " +
-            "    ELSE (m.system = :system AND m.version IS NULL) " +
-            "END")
+    @Query(value =
+            "SELECT m FROM Mapping m " +
+            "WHERE m.system = :system " +
+                    "AND (m.version = :version " +
+                    "OR (m.version IS NULL AND NOT EXISTS (" +
+                    "SELECT m2 FROM Mapping m2 " +
+                    "WHERE m2.system = :system " +
+                    "AND m2.version = :version)))")
     Optional<Mapping> findAny(@Param("system") String system, @Param("version") String version);
 }

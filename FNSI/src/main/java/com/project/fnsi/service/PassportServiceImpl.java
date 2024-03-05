5f6efded28a4dfe89aa7da25be86df3012e49156
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-03-05 21:02:12 +0300

    issue 34 Разработали логику получения полей маппинга справочника

--- FNSI/src/main/java/com/project/fnsi/service/PassportServiceImpl.java
+++ FNSI/src/main/java/com/project/fnsi/service/PassportServiceImpl.java
@@ -3,22 +3,35 @@ package com.project.fnsi.service;
 import com.project.fnsi.dao.PassportRepository;
 import com.project.fnsi.entity.Passport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PassportServiceImpl implements PassportService {
 
     private final PassportRepository passportRepository;
+    private final RestTemplate restTemplate;
+    @Value("${fnsi.key}")
+    private String key;
     @Autowired
-    public PassportServiceImpl(PassportRepository passportRepository) {
+    public PassportServiceImpl(PassportRepository passportRepository, RestTemplate restTemplate) {
         this.passportRepository = passportRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Override
     @Transactional
     public Passport getPassportBySystemAndVersion(String system, String version) {
-        return passportRepository.findOne(system, version).orElse(null);
+        Passport passport = passportRepository.findOne(system, version).orElse(null);
+        if (passport != null){
+            return passport;
+        }
+        String data = restTemplate.getForObject("http://nsi.rosminzdrav.ru/port/rest/passport?userKey="+key+"&identifier="+system+"&version="+version, String.class);
+       // todo сделать проверку на отсутствие данных
+        passport = new Passport(null, system, version, data);
+        return passportRepository.save(passport);
     }
 
     @Override

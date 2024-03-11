867e4ceebb1205a4de83a38c75648e9e5262d20a
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-03-11 06:28:48 +0300

    issue 43 Заменил RuntimeException на кастомные исключения там, где это необходимо. Создал ExeptionHandler.

---
+++ FNSI/src/main/java/com/project/fnsi/controllers/ExeptionController.java
@@ -0,0 +1,33 @@
+package com.project.fnsi.controllers;
+
+import com.project.fnsi.exeptions.CreatingMappingException;
+import com.project.fnsi.exeptions.AppExeptionData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class ExeptionController {
+    @ExceptionHandler
+    public ResponseEntity<AppExeptionData> handleException(CreatingMappingException exception) {
+        AppExeptionData data = new AppExeptionData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<AppExeptionData> handleException(Exception exception) {
+        AppExeptionData data = new AppExeptionData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler
+    public ResponseEntity<AppExeptionData> handleException(EntityNotFoundException exception) {
+        AppExeptionData data = new AppExeptionData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+}

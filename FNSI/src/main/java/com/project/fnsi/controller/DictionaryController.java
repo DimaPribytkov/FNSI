84ff736fc0ae2b43e027e3239a5f3d9d0c14b3ba
Yunir <spire47911@yandex.ru>
2024-03-13 21:32:59 +0500

    issue #15

---
+++ FNSI/src/main/java/com/project/fnsi/controller/DictionaryController.java
@@ -0,0 +1,35 @@
+package com.project.fnsi.controller;
+
+import com.project.fnsi.entity.Dictionary;
+import com.project.fnsi.service.DictionaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/dictionaries")
+public class DictionaryController {
+
+    private final DictionaryService dictionaryService;
+
+    @Autowired
+    public DictionaryController(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
+    @GetMapping("/{system}/{version}/{code}")
+    public Dictionary getDictionary(@PathVariable String system, @PathVariable String version, @PathVariable String code) {
+        return dictionaryService.getDictionary(system, version, code);
+    }
+
+    @PutMapping("/")
+    public Dictionary updateDictionary(@RequestBody Dictionary dictionary) {
+        return dictionaryService.updateDictionary(dictionary);
+    }
+
+
+    @DeleteMapping("/{system}/{version}/{code}")
+    public void deleteDictionary(@PathVariable String system, @PathVariable String version, @PathVariable String code) {
+        dictionaryService.deleteDictionary(system, version, code);
+    }
+}
